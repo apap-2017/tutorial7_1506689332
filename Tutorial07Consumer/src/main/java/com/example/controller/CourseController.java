e8599ce5bf3e9905060597ef05c8af875795c178
Rifka Nur Fadhilah <rifka.nur@ui.ac.id>
2017-11-03 00:57:14 +0700

    add latihan 4

--- Tutorial07Consumer/src/main/java/com/example/controller/CourseController.java
+++ Tutorial07Consumer/src/main/java/com/example/controller/CourseController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.model.CourseModel;
+import com.example.model.StudentModel;
 import com.example.service.CourseService;
 
 @Controller
@@ -30,5 +33,14 @@ public class CourseController
             model.addAttribute("pageTitle", "Course Not Found");
             return "course-not-found";
         }
+    }
+    
+    @RequestMapping("/course/viewall")
+    public String view (Model model)
+    {
+        List<CourseModel> courses = courseDAO.selectAllCourses();
+        model.addAttribute ("courses", courses);
+        model.addAttribute("pageTitle", "View All Courses");
+        return "viewall-courses";
     }
 }
--- Tutorial07Consumer/src/main/java/com/example/service/CourseService.java
+++ Tutorial07Consumer/src/main/java/com/example/service/CourseService.java
@@ -1,8 +1,12 @@
 package com.example.service;
 
+import java.util.List;
+
 import com.example.model.CourseModel;
 
 public interface CourseService
 {
     CourseModel selectCourse (String npm);
+    
+    List<CourseModel> selectAllCourses();
 }
